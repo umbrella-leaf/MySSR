149b1b8bc8c70b472d17fc0f48e4bd8bd73fa9ea
Liu Zhongnuo <2313678365@qq.com>
2023-05-28 14:32:13 +0800

    修正服务端文件bug

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -2,15 +2,19 @@ import express from "express";
 import childProcess from "child_process";
 import {renderToString} from "react-dom/server";
 import Home from "@/pages/Home";
+import path from "path";
 
 const app = express();
 const content = renderToString(<Home />)
+
+app.use(express.static(path.resolve(process.cwd(), "client_build")));
 
 app.get("*", (req, res) => {
   res.send(`
     <html>
       <body>
         <div id="root">${content}</div>
+        <script src="/index.js"></script>
        </body>
      </html>
   `);
