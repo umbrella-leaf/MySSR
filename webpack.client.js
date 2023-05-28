c48e0ab7da94ceabb6f99c1bcc3915a8be89e18e
Liu Zhongnuo <2313678365@qq.com>
2023-05-28 14:30:43 +0800

    修正客户端webpack配置

--- webpack.client.js
+++ webpack.client.js
@@ -4,7 +4,7 @@ const baseConfig = require("./webpack.base");
 
 module.exports = merge(baseConfig, {
   mode: "development",
-  entry: "./src/client/index.ts",
+  entry: "./src/client/index.tsx",
   output: {
     filename: "index.js",
     path: path.resolve(process.cwd(), "client_build"),
